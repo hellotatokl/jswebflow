f384fa5db96120b69a19b6d6a27b0d9049593f14
hellotatokl <93349396+hellotatokl@users.noreply.github.com>
2023-05-24 22:31:25 +0300

    Update effect.js

--- effect.js
+++ effect.js
@@ -1157,12 +1157,12 @@ function HSVtoRGB (h, s, v) {
     t = v * (1 - (1 - f) * s);
 
     switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+        case 0: r = v, g = v, b = v; break;
+        case 1: r = v, g = v, b = v; break;
+        case 2: r = v, g = v, b = v; break;
+        case 3: r = v, g = v, b = v; break;
+        case 4: r = v, g = v, b = v; break;
+        case 5: r = v, g = v, b = v; break;
     }
 
     return {
